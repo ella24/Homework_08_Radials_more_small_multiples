3b9df23267b0833a1044e05260ca0b53266c710f
ella <coral.negron@outlook.com>
2018-10-14 15:41:32 -0500

    updates

--- src/chart-3.js
+++ src/chart-3.js
@@ -4,7 +4,7 @@ var margin = { top: 30, left: 30, right: 30, bottom: 30 }
 var height = 400 - margin.top - margin.bottom
 var width = 780 - margin.left - margin.right
 
-var svg = d3
+let svg = d3
   .select('#chart-3')
   .append('svg')
   .attr('height', height + margin.top + margin.bottom)
@@ -12,11 +12,86 @@ var svg = d3
   .append('g')
   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
+let pie = d3
+  .pie()
+  .value(1 / 12)
+  .sort(null)
+
+let radiusScale = d3
+  .scaleLinear()
+  .domain([0, 100])
+  .range([0, 200])
+
+let arc = d3
+  .arc()
+  .innerRadius()
+  .outerRadius(d => radiusScale(d.data.high_temp))
+
+let colorScale = d3
+  // different type of scale
+  .scaleQuantize()
+  .range(['#e5f5e0', '#a1d99b', '#31a354', '#feb24c', '#f03b20'])
+
+let arcLabel = d3
+  .arc()
+  .innerRadius(0)
+  .outerRadius(d => radiusScale(d.data.high_temp) + 20)
 
 d3.csv(require('./data/ny-temps.csv'))
   .then(ready)
   .catch(err => console.log('Failed on', err))
 
 function ready(datapoints) {
+  let container = svg
+    .append('g')
+    .attr('transform', `translate(${width / 2},${height / 2})`)
 
+  let alltemp = datapoints.map(d => +d.high_temp)
+  colorScale.domain(d3.extent(alltemp))
+
+  container
+    .selectAll('.path-chart')
+    .data(pie(datapoints))
+    .enter()
+    .append('path')
+    .attr('class', 'path-chart')
+    .attr('d', d => arc(d))
+    .attr('fill', d => colorScale(d.data.high_temp))
+
+  container
+    .append('text')
+    .text('NYC high temperatures, by month')
+    .attr('text-anchor', 'middle')
+    .attr('font-size', 20)
+    .attr('dy', -150)
+    .attr('font-weight', 400)
+
+  container
+    .selectAll('.pie-text')
+    .data(pie(datapoints))
+    .enter()
+    .append('text')
+    .text(d => d.data.month_name)
+    .attr('x', d => {
+      return arcLabel.centroid(d)[0]
+    })
+    .attr('y', d => {
+      return arcLabel.centroid(d)[1]
+    })
+    .attr('transform', function(d) {
+      return 'translate(' + arcLabel.centroid(d) + ')'
+    })
+    .attr('text-anchor', function(d) {
+      if (d.startAngle > Math.PI) {
+        return 'end'
+      } else {
+        return 'start'
+      }
+    })
+
+  container
+    .append('circle')
+    .attr('r', 2.5)
+    .attr('x', 0)
+    .attr('y', 0)
 }
